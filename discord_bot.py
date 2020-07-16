060b26bd2b4c12e4e0faa66de86869d0df6bd91d
Dylan Sechet <dylan.sechet@student-cs.fr>
2020-07-16 20:49:26 +0200

    Fixed regex * error

--- discord_bot.py
+++ discord_bot.py
@@ -22,9 +22,9 @@ VARS = {} # per-channel local variables
 KEYWORDS = [
 [("lord english", "caliborn", "lord of time", "l0rd of time", "l*rd of time"), ("He who is already here shall come when this Universe ends.","Nobody can outrun my master.")],
 [("time lord", "the doctor"), ("My master is the one and only Lord of Time.",)],
-[("time l0rd", "time l*rd"), ("How dare you try to censor my master's name?",)],
+[("time l0rd", "time l\*rd"), ("How dare you try to censor my master's name?",)],
 [(r"\bhonk\b",), ("http://tiny.cc/hOnK","hOnK")],
-[("hussie", "h*ssie", "andrew", "the huss"), ("https://imgur.com/ZMszE75","https://imgur.com/tmGONPE")],
+[("hussie", "h\*ssie", "andrew", "the huss"), ("https://imgur.com/ZMszE75","https://imgur.com/tmGONPE")],
 [("the mayor",), ("I must admit, even I am quite fond of him.",)],
 [("lil cal",), ("The poor thing, treated like nothing more than a puppet.", "He is already here.")],
 [(r"\bcal\b",), ("Such familiarity towards my master is quite insulting.", "You shall pay your irreverence with your blood.", "https://imgur.com/ceGQm3g")],
